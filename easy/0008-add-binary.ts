9d4b817553bff7c638b86c222d783edbd8c9dd09
vepifanio <vepifanio1@gmail.com>
2024-01-12 10:00:28 -0300

    0008

--- easy/0008-add-binary.ts
+++ easy/0008-add-binary.ts
@@ -1,50 +1,55 @@
-// function addBinary(a: string, b: string): string {
-//   const result: number[] = []
-
-//   const longestBinary = a.length > b.length ? a : b
-//   const shortestBinary = longestBinary === a ? b : a
-
-//   const longestBinaryDigits = longestBinary.split('')
-//   const shortestBinaryDigits = shortestBinary.split('')
-
-//   const lenghtDifference = longestBinary.length - shortestBinary.length
-
-//   const carry = 0
-
-//   for (let i = longestBinary.length - 1; i >= 0; i--) {
-//     const firstDigit = Number(longestBinaryDigits[i])
-//     const secondDigit = Number(shortestBinaryDigits[i - lenghtDifference]) || 0
-
-//     let resultedDigit
-
-//     if (firstDigit === 0 && secondDigit === 0) {
-//       resultedDigit = 0
-//     } else if (firstDigit === 1 && secondDigit === 1) {
-//       resultedDigit = 0
-//     } else {
-//       resultedDigit = 1
-//     }
-
-//     console.log(resultedDigit)
-//   }
-
-//   return result.join('')
-// }
-
-// console.log(addBinary('111', '101'))
-
 function addBinary(a: string, b: string): string {
-  let result = ''
+  const result: string[] = []
 
-  let carry = 0
-  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
-    const num1 = parseInt(a[i] ?? '0')
-    const num2 = parseInt(b[j] ?? '0')
-    const sum = num1 + num2 + carry
+  const longestBinary = a.length > b.length ? a : b
+  const shortestBinary = longestBinary === a ? b : a
 
-    carry = sum > 1 ? 1 : 0
-    result = (sum % 2 === 1 ? '1' : '0') + result
+  const lenghtDifference = longestBinary.length - shortestBinary.length
+
+  let carry = '0'
+
+  for (let i = longestBinary.length - 1; i >= 0; i--) {
+    const firstDigit = longestBinary.charAt(i)
+    const secondDigit = shortestBinary.charAt(i - lenghtDifference) || '0'
+
+    let addition
+    const currentCarry = carry
+
+    if (firstDigit === '0' && secondDigit === '0') {
+      addition = '0'
+      carry = '0'
+    } else if (
+      (firstDigit === '1' && secondDigit === '0') ||
+      (firstDigit === '0' && secondDigit === '1')
+    ) {
+      addition = '1'
+      carry = '0'
+    } else {
+      addition = '0'
+      carry = '1'
+    }
+
+    if (addition === '0' && currentCarry === '0') {
+      addition = '0'
+    } else if (
+      (addition === '1' && currentCarry === '0') ||
+      (addition === '0' && currentCarry === '1')
+    ) {
+      addition = '1'
+    } else {
+      addition = '0'
+      carry = '1'
+    }
+
+    result.push(addition)
   }
 
-  return carry ? '1' + result : result
+  if (carry === '1') {
+    result.push('1')
+  }
+
+  return result.reverse().join('')
 }
+
+console.log(addBinary('11', '1')) // 100
+console.log(addBinary('1010', '1011')) // 10101
