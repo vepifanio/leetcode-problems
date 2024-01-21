9f9e6b1f1bb9f3395fdddff5b80ec4bdf44b1e2c
vepifanio <vepifanio1@gmail.com>
2024-01-21 19:32:29 -0300

    0011

---
+++ easy/0011-remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,45 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+
+  toArray() {
+    const valuesArray: number[] = []
+    let head = this
+
+    while (head !== null) {
+      valuesArray.push(head.val)
+      head = head.next
+    }
+
+    return valuesArray
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (head === null) {
+    return null
+  }
+
+  let currentNext = head.next
+
+  while (head.val === currentNext?.val && currentNext !== null) {
+    currentNext = currentNext.next
+  }
+
+  head.next = deleteDuplicates(currentNext)
+
+  return head
+}
+
+const list1 = new ListNode(1, new ListNode(1, new ListNode(2)))
+console.log(deleteDuplicates(list1).toArray()) // [1, 2]
+
+const list2 = new ListNode(
+  1,
+  new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))),
+)
+console.log(deleteDuplicates(list2).toArray()) // [1, 2, 3]
