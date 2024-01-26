066e7070b3c59c35b1e201e3b569d586e7a5f767
vepifanio <vepifanio1@gmail.com>
2024-01-26 18:54:40 -0300

    0016

---
+++ easy/0016-max-depth.ts
@@ -0,0 +1,10 @@
+function maxDepth(root: TreeNode | null): number {
+  if (root === null) {
+    return 0
+  }
+
+  const leftHeight = maxDepth(root.left)
+  const rightHeight = maxDepth(root.right)
+
+  return Math.max(leftHeight, rightHeight) + 1
+}
